3621486dc29af1274026a68c87c8a09a988762dc
notarthurphan <nahpruhtraton@gmail.com>
2019-04-19 01:59:21 -0700

    test commit 2

--- Destination.java
+++ Destination.java
@@ -1,10 +1,9 @@
 // written by Arthur Phan for CS48- Organized (All-Stars)
-// class Destination is a node, to be used for linked-list 
-
+// class Destination is a node, to be used for linked-list
+// test commit 2
 public class Destination{
     String Method;
     String Source;
-    // comment
     int Position;
     Destination Next;
 }
