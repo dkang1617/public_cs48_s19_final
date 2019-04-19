1d953257ca49925765124b23e8968f99803f24b3
notarthurphan <nahpruhtraton@gmail.com>
2019-04-19 02:45:42 -0700

    node class should be done; need to work on LL + api

--- Destination.java
+++
@@ -1,9 +0,0 @@
-// written by Arthur Phan for CS48- Organized (All-Stars)
-// class Destination is a node, to be used for linked-list
-// test commit 2
-public class Destination{
-    String Method;
-    String Source;
-    int Position;
-    Destination Next;
-}
---
+++ implementation.java
@@ -0,0 +1,21 @@
+// written by Arthur Phan for CS48- Organized (All-Stars)
+
+// implementation notes
+// frontend- dragout for destination names
+// destination names allowed (checked on frontend)
+import destination.DestNode;
+import java.util.*;
+
+
+public class test{
+    public static void main(String args[]){
+        //Creating object of class linked list
+        LinkedList<DestNode> DestList = new LinkedList<DestNode>();
+
+        DestList.add(DestNode("Girvetz", "Biking"))
+        DestList.add(DestNode("Manzi", "Walking"))
+        System.out.println("DestList: " + DestList);
+
+
+    }
+}
