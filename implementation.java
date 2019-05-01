309ece9d0259353cf8a510c037cb3a5fa0a45150
notarthurphan <nahpruhtraton@gmail.com>
2019-05-01 16:46:02 -0700

    added comments for implementation; will change

--- implementation.java
+++ implementation.java
@@ -10,41 +10,50 @@ import java.util.*;
 
 public class implementation{
     public static void main(String args[]){
-        // Creating object of class linked list
+        /* Creating object of class linked list */
         LinkedList<DestNode> DestList = new LinkedList<DestNode>();
         Scanner s = new Scanner(System.in);
-        int c = 1;
 
-        while(c == 1 && DestList.size() != 10){ // list must not exceed 10, and check if they want more nodes
+        /* Boolean int for whether to stop asking for input */
+        int notDone = 1;
 
+        /* List must not exceed 10, and check if user wants more nodes */
+        while(notDone == 1 && DestList.size() != 10){
             System.out.print("Enter a destination, or input nothing if no more destinations are wanted: ");
             String tempSrc = s.nextLine();
-            if(Objects.equals(tempSrc, new String(""))){
-                c = 0;
-            }
-            if(c == 1){ // only add node if tempSrc != 1
+
+            /* checks in user inputted "" to stop adding nodes */
+            notDone = !(Objects.equals(tempSrc, new String("")));
+
+            /* only add node if tempSrc != "" */
+            if(notDone == 1){
                 System.out.print("Enter a transportation method: ");
                 String tempMeth = s.nextLine();
+
+                /* default method is walking */
                 if(Objects.equals(tempMeth, new String(""))){
-                    tempMeth = "walking"; // default is walking
-		    System.out.println("      -defaulted to walking-");
+                    tempMeth = "walking";
+                    System.out.println("      -defaulted to walking-");
                 }
+                /* create and add node */
                 DestNode tempNode = new DestNode(tempSrc, tempMeth);
                 DestList.add(tempNode);
             }
         }
 
-        if(DestList.size() == 10){
+        /* warning message for list length 10 */
+        if(DestList.size() == 10)
             System.out.println("The list can only contain 10 destinations. ");
-        }
+
+        /* only print for nonempty list */
         if(DestList.size() != 0){
             System.out.println("Your list is: ");
             for(int i = 0; i < DestList.size(); i++){
                 DestNode tempN = DestList.get(i);
                 System.out.println(" " + (i + 1) + ". " + tempN.getSource() + ", " + tempN.getMethod());
-                // loop for printing list
             }
         }
+        /* message if no items were added */
         else{
             System.out.println("No locations were added to the list.");
         }
