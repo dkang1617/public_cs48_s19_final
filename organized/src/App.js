0369820d3bb02e63cd0a71c414d93450ab7ad307
Juan Gonzalez <juan_gonzalez@umail.ucsb.edu>
2019-05-08 13:42:27 -0700

    Commented out the import of config to get test to work

--- organized/src/App.js
+++ organized/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { GoogleLogin } from 'react-google-login';
-import config from './config.json'
+//import config from './config.json'
 import jsontest from './jsontest'
 import JsonParser from './JsonParser'
 
