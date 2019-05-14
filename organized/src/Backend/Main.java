1a942c4558c6ee0b3ae7d0d5a652ebdf1083e081
Krishna Nanda <krishnananda@ucsb.edu>
2019-05-14 13:36:49 -0700

    Update Main.java

--- organized/src/Backend/Main.java
+++ organized/src/Backend/Main.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+      get("/hello", (req, res) -> "Hello World");
+      get("/math",(req,res) -> (4+4));
     }
 }
