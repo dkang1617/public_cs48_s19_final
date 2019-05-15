82a41f790dbf0ac6e9834bb2bce7894b64f72f89
Krishna Nanda <krishnananda@ucsb.edu>
2019-05-15 16:50:35 -0700

    Update Main.java

--- organized/src/Backend/Main.java
+++ organized/src/Backend/Main.java
@@ -1,9 +1,46 @@
-package com.company;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+
 import static spark.Spark.*;
 
-public class Main {
+public class servertest {
     public static void main(String[] args) {
-      get("/hello", (req, res) -> "Hello World");
-      get("/math",(req,res) -> (4+4));
-    }
-}
+        port(9000);
+        get("/hello",(req,res) -> ("Hello World"));
+        get("/json",(req,res) -> {
+
+                        URL url = new URL("https://raw.githubusercontent.com/dkang1617/myjsontest/master/db.json");
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setRequestMethod("GET");
+                        conn.setRequestProperty("Accept", "application/json");
+
+                        if (conn.getResponseCode() != 200) {
+                            throw new RuntimeException("Failed : HTTP error code : "
+                                    + conn.getResponseCode());
+                        }
+
+                        BufferedReader br = new BufferedReader(new InputStreamReader(
+                                (conn.getInputStream())));
+                        String output = "";
+                        while(br.readLine() != null) {
+                           output += br.readLine();
+                       }
+                        conn.disconnect();
+
+                        return output;
+
+
+
+        });
+
+            }
+
+
+        }
