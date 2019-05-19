74427028dacd7e6611d2ab9733d598cae746b736
dkang1617 <35702623+dkang1617@users.noreply.github.com>
2019-05-19 15:12:21 -0700

    final update

--- organized/src/JsonParser.test.js
+++ organized/src/JsonParser.test.js
@@ -8,22 +8,22 @@ it('Is test', () => {
 });
 
 it('Test getStartTime', () => {
-	const testJson = new JsonParser(jsontest);
+	const testJson = new JsonParser(jsontest.Courses);
 	expect(testJson.getStartTime(0)).toEqual("15:00:00");
 });
 
 it('Test getEndTime', () => {
-	const testJson = new JsonParser(jsontest);
+	const testJson = new JsonParser(jsontest.Courses);
 	expect(testJson.getEndTime(0)).toEqual("16:15:00");
 });
 
 it('Test getID', () => {
-	const testJson = new JsonParser(jsontest);
+	const testJson = new JsonParser(jsontest.Courses);
 	expect(testJson.getID(0)).toEqual("12345");
 });
 
 it('Test getDate', () => {
-	const testJson = new JsonParser(jsontest);
+	const testJson = new JsonParser(jsontest.Courses);
 	expect(testJson.getDate(0)).toEqual("2019-05-08T");
 })
 	
