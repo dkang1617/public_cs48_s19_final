27ad49ec1083c8de95225be21f8c27cacf144257
dkang1617 <35702623+dkang1617@users.noreply.github.com>
2019-05-19 14:58:05 -0700

    update

--- organized/src/JsonParser.test.js
+++ organized/src/JsonParser.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import JsonParser from './JsonParser';
 import jsontest from './jsontest';
 
 it('Is test', () => {
