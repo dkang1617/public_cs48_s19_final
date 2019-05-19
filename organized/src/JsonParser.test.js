35aedfc0edadc1574d00dde124e77a725ed5417b
dkang1617 <35702623+dkang1617@users.noreply.github.com>
2019-05-19 14:55:17 -0700

    wrote tests for jsonparser

--- organized/src/JsonParser.test.js
+++ organized/src/JsonParser.test.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import jsontest from './jsontest';
 
 it('Is test', () => {
-		expect("fakeName").toEqual("fakeName");
-
+	expect("fakeName").toEqual("fakeName");
 });
+
+it('Test getStartTime', () => {
+	const testJson = new JsonParser(jsontest);
+	expect(testJson.getStartTime(0)).toEqual("15:00:00");
+});
+
+it('Test getEndTime', () => {
+	const testJson = new JsonParser(jsontest);
+	expect(testJson.getEndTime(0)).toEqual("16:15:00");
+});
+
+it('Test getID', () => {
+	const testJson = new JsonParser(jsontest);
+	expect(testJson.getID(0)).toequal("12345");
+});
+
+it('Test getDate', () => {
+	const testJson = new JsonParser(jsontest);
+	expect(testJson.getDate(0)).toEqual("2019-05-08T");
+});
+	
