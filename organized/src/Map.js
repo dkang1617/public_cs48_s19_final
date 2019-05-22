e26aa3388a047cad9fe40cbb96197096c59c2f97
notarthurphan <nahpruhtraton@gmail.com>
2019-05-22 14:41:11 -0700

    array method added

--- organized/src/Map.js
+++ organized/src/Map.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import JsonParser from './JsonParser';
 
 class Map extends Component {
+
 	/* array for storing iframe strings */
 	addToArray = async (e) =>{
-
 		/* dkang = Daniels' online API */
 		// const apiCall = await fetch("https://my-json-server.typicode.com/dkang1617/myjsontest/Courses",{
 
@@ -21,38 +21,36 @@ class Map extends Component {
 			couseCount counts how many courses there are
 		*/
 
+		/* console debug statement */
 		const data = await apiCall.json();
 		console.log(data);
 		console.log(data.Courses);
 		const jsonParser = new JsonParser(data.Courses);
 		const courseCount = data.Courses.length;
 		console.log(courseCount);
-		/* console debug statement */
-		for(var i = 0 ; i < courseCount; i++){
-			console.log(jsonParser.getID(i)+' Meets at:'+jsonParser.getStartTime(i)+' Ends at:'+jsonParser.getEndTime(i));
-			console.log(jsonParser.getDate(i))
-			console.log(jsonParser.getRepeat(i))
-		}
 
-		/* add all course buildings to array */
+		/* to call variables from input use e.target.elements.<name>.value */
+		var buildingArray = [e.target.elements.srcBuilding.value]
+		/* add all course buildings to array from jsonParser object */
 		for(var i = 0; i < courseCount; i++){
-
+			buildingArray.push(jsonParser.getBuilding(i));
 		}
 
-		/* this is where the events are being added to calendar */
-		const apiCall = await fetch(  "https://www.googleapis.com/calendar/v3/calendars/"+this.state.email+"/events",{
-			method:"post",
-			body : JSON.stringify(event),
-			headers:{
-				'Content-Type': 'application/json ; charset=UTF-8',
-				Authorization: "Bearer "+this.state.token,
-			}
-		})
+		/* console debug statement */
+		for(var i = 0 ; i < buildingArray.length; i++){
+			console.log(buildingArray[i]);
+		}
+
+
 	}
 
 	render(){
 		return(
 			<div>
+			/* 	website interface has a text input, storing in srcBuilding
+				the transportation method defaults to walking, but can be checked to biking
+				and is stored in transMeth
+			*/
 				<form onSubmit={this.addToArray}>
 					<input type = "text" name="srcBuilding" placeholder= "Where the source is before going to class"/>
 					<input type = "radio" name="transMeth" value= "walking" checked> Walking <br>
