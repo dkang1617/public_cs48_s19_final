34afb322904b29c41114d882d0106d2b7068dfd7
Juan Gonzalez <juan_gonzalez@umail.ucsb.edu>
2019-05-22 22:24:34 -0700

    Fixed some JSX stuff and addArray

--- organized/src/Map.js
+++ organized/src/Map.js
@@ -7,11 +7,13 @@ class Map extends Component {
 	// npm start to run webpage
 
 	addToArray = async (e) =>{
+		e.preventDefault();
+		const srcBuilding = e.target.elements.srcBuilding.value;
 		/* dkang = Daniels' online API */
-		// const apiCall = await fetch("https://my-json-server.typicode.com/dkang1617/myjsontest/Courses",{
+		const apiCall = await fetch("https://my-json-server.typicode.com/dkang1617/myjsontest/Courses",{
 
 		/* local host = Krishna's json stuff - calls java */
-		const apiCall = await fetch("http://localhost:9000/json",{
+		// const apiCall = await fetch("http://localhost:9000/json",{
 		/* method is GET call, mode specifies which mode - cors is the secure route */
 			method:'get',
 			mode:'cors'
@@ -27,12 +29,12 @@ class Map extends Component {
 		const data = await apiCall.json();
 		console.log(data);
 		console.log(data.Courses);
-		const jsonParser = new JsonParser(data.Courses);
-		const courseCount = data.Courses.length;
+		const jsonParser = new JsonParser(data);
+		const courseCount = data.length;
 		console.log(courseCount);
 
 		/* to call variables from input use e.target.elements.<name>.value */
-		var buildingArray = [e.target.elements.srcBuilding.value]
+		var buildingArray = [srcBuilding];
 
 		/* add all course buildings to array from jsonParser object */
 		for(var i = 0; i < courseCount; i++){
@@ -51,26 +53,29 @@ class Map extends Component {
 
 	render(){
 		return(
+			<div>
 			{/* website interface has a text input, storing in srcBuilding
 				the transportation method defaults to walking, but can be checked to biking
 				and is stored in transMeth
-			*/}
+			
 
 			// 1. necessary: get the submit form to work
 
 			// 2. return new tab of website
 
-			// 3. load the embedded google maps on that page
+			// 3. load the embedded google maps on that page*/}
 			<div>
-				<form action ={this.addToArray}>
+				<form onSubmit ={this.addToArray}>
 					<input type = "text" name="srcBuilding" placeholder= "Starting Destination"/><br/>
-					<input type = "radio" id="walking" name="transMeth" value= "walking" checked/><br/>
-					<label for = "walking">Walking</label>
-					<input type = "radio" id="biking" name="transMeth" value= "biking"/><br/>
-					<input type = "submit" value = "Submit" />
+					<input type = "radio" id="walking" name="transMeth" value= "walking" checked/>
+					<label for = "walking">Walking</label><br/>
+					<input type = "radio" id="biking" name="transMeth" value= "biking"/>
+					<label for = "biking">Biking</label><br/>
+					<button>Submit</button>
 				</form>
 			</div>
 			</div>
+
 		);
 	}
 }
