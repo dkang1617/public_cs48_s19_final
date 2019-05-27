7abb53e87201bd4fc8a40a61d8be876a5468d1d5
notarthurphan <nahpruhtraton@gmail.com>
2019-05-26 18:44:15 -0700

    groundwork laid for manual add- writing function now

--- organized/src/Map.js
+++ organized/src/Map.js
@@ -76,6 +76,14 @@ class Map extends Component {
         }
 	}
 
+	manualAdd = async (e) =>{
+		e.preventDefault();
+
+		/* initialized values using user input values and a helper integer */
+		const srcBuilding = e.target.elements.srcBuilding.value;
+		const transMeth = e.target.elements.transMeth.value;
+		var alignCounter = 5;
+	}
     render(){
 		return(
 			<div>
@@ -95,7 +103,16 @@ class Map extends Component {
                 // &mode=transMeth                         */}
                 </div>
                     <div>
-                    <div>
+					<text>
+						Note: to use this functionality, you must allow popups in your browser
+					</text>
+					<br/><br/><br/>
+					<text>
+						Automated Class Pathing Instructions:
+					</text> <br/>
+					<text>
+						Insert your starting location (e.g, your dorm room) and select the method in which you want to go to class.
+					</text>
         				<form onSubmit ={this.addToArray}>
         					<input type = "text" name="srcBuilding" placeholder= "Starting Destination"/><br/>
         					<input type = "radio" id="walking" name="transMeth" value= "walking" checked/>
@@ -105,8 +122,76 @@ class Map extends Component {
         					<button>Submit</button>
         				</form>
                     </div>
-                    <iframe id="myIframe" src="" height="250px"  width="100%" scrolling="yes" frameborder="0"></iframe>
+					<br/><br/><br/><br/><br/>
+					<text>
+						Manual Class Pathing Instructions:
+					</text> <br/>
+					<text>
+						Insert up to 10 location and select the method in which you want to go to each of these destinations.
+					</text>
+					<div>
+        				<form onSubmit ={this.manualAdd}>
+        					<input type = "text" name="location1" placeholder= "Starting Destination"/><br/><br/>
+
+							<input type = "text" name="location2" placeholder= "2nd Destination"/><br/>
+							<input type = "radio" id="walking" name="to2nd" value= "walking" checked/>
+							<label for = "walking">Walking</label>
+							<input type = "radio" id="biking" name="to2nd" value= "bicycling"/>
+							<label for = "biking">Biking</label><br/><br/>
+
+							<input type = "text" name="location3" placeholder= "3rd Destination"/><br/>
+							<input type = "radio" id="walking" name="to3rd" value= "walking" checked/>
+							<label for = "walking">Walking</label>
+							<input type = "radio" id="biking" name="to3rd" value= "bicycling"/>
+							<label for = "biking">Biking</label><br/><br/>
+
+							<input type = "text" name="location4" placeholder= "4th Destination"/><br/>
+							<input type = "radio" id="walking" name="to4th" value= "walking" checked/>
+							<label for = "walking">Walking</label>
+							<input type = "radio" id="biking" name="to4th" value= "bicycling"/>
+							<label for = "biking">Biking</label><br/><br/>
+
+							<input type = "text" name="location5" placeholder= "5th Destination"/><br/>
+							<input type = "radio" id="walking" name="to5th" value= "walking" checked/>
+							<label for = "walking">Walking</label>
+							<input type = "radio" id="biking" name="to5th" value= "bicycling"/>
+							<label for = "biking">Biking</label><br/><br/>
+
+							<input type = "text" name="location6" placeholder= "6th Destination"/><br/>
+							<input type = "radio" id="walking" name="to6th" value= "walking" checked/>
+							<label for = "walking">Walking</label>
+							<input type = "radio" id="biking" name="to6th" value= "bicycling"/>
+							<label for = "biking">Biking</label><br/><br/>
+
+							<input type = "text" name="location7" placeholder= "7th Destination"/><br/>
+							<input type = "radio" id="walking" name="to7th" value= "walking" checked/>
+							<label for = "walking">Walking</label>
+							<input type = "radio" id="biking" name="to7th" value= "bicycling"/>
+							<label for = "biking">Biking</label><br/><br/>
+
+							<input type = "text" name="location8" placeholder= "8th Destination"/><br/>
+							<input type = "radio" id="walking" name="to8th" value= "walking" checked/>
+							<label for = "walking">Walking</label>
+							<input type = "radio" id="biking" name="to8th" value= "bicycling"/>
+							<label for = "biking">Biking</label><br/><br/>
+
+							<input type = "text" name="location9" placeholder= "9th Destination"/><br/>
+							<input type = "radio" id="walking" name="to9th" value= "walking" checked/>
+							<label for = "walking">Walking</label>
+							<input type = "radio" id="biking" name="to9th" value= "bicycling"/>
+							<label for = "biking">Biking</label><br/><br/>
+
+							<input type = "text" name="location10" placeholder= "10th Destination"/><br/>
+							<input type = "radio" id="walking" name="to10th" value= "walking" checked/>
+							<label for = "walking">Walking</label>
+							<input type = "radio" id="biking" name="to10th" value= "bicycling"/>
+							<label for = "biking">Biking</label><br/><br/>
+
+        					<button>Submit</button>
+        				</form>
                     </div>
+
+
 			</div>
 
 		);
